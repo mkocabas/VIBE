90835512ea758189c73a8b1180335fee6d86a1bd
Muhammed Kocabas <muhammed.kocabas@outlook.com>
2020-03-04 10:18:52 +0100

    update pip install

--- install_pip.sh
+++ install_pip.sh
@@ -6,5 +6,5 @@ echo "Activating virtual environment"
 
 source $PWD/vibe-env/bin/activate
 
-$PWD/vibe-env/bin/pip install numpy torch torchvision
+$PWD/vibe-env/bin/pip install numpy==1.17.5 torch==1.4.0 torchvision==0.5.0
 $PWD/vibe-env/bin/pip install -r requirements.txt
